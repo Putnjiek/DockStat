49f66e760b975d0e399830c6499f29d36b8191bc
ItsNik <106100177+Its4Nik@users.noreply.github.com>
2024-08-26 05:55:50 +0200

    Update entrypoint.sh
    reqrite config.json on every container start, to make sure that the right config is used

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,18 @@ if [[ -z "${DEFAULT_THEME}" ]]; then
     DEFAULT_THEME="dracula"
 fi
 
+if [[ ! -f /app/build/config.json ]]; then
+    touch /app/build/config.json
+else
+    rm touch /app/build/config.json
+    touch touch /app/build/config.json
+fi
+
 echo "============ DockStat ============"
 echo "API_URL:       ${API_URL}"
 echo "DEFAULT_THEME: ${DEFAULT_THEME}"
 echo "SECRET:        ${SECRET}"
 echo "============ DockStat ============"
-
-touch /app/build/config.json
 
 echo "
 {
