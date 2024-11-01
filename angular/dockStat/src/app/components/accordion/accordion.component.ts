9ce4bfa84bca77e478ced7b44e38f33e699b096b
Anni Rauch <annkathrin_rauch@icloud.com>
2024-11-01 17:14:31 +0100

    improved accordion styling

--- angular/dockStat/src/app/components/accordion/accordion.component.ts
+++ angular/dockStat/src/app/components/accordion/accordion.component.ts
@@ -1,31 +1,17 @@
-import { Component, input, model, OnDestroy, signal } from '@angular/core';
+import { Component, input, model, signal } from '@angular/core';
 import { Container } from '../../interfaces/container';
 import { ServerStatus } from '../../interfaces/serverStatus';
-import { ContainerService } from '../../services/container.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
 })
-export class AccordionComponent implements OnDestroy {
+export class AccordionComponent {
 
   containers = model.required<Container[]>();
 
   serverStatus = input.required<ServerStatus>();
 
   isAccordionOpen = signal(true);
-
-  subscription = new Subscription();
-
-  constructor(private containerService: ContainerService) {
-    this.subscription.add(this.containerService.allContainers$.subscribe(allContainers => {
-      this.containers.set(allContainers);
-    }))
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
