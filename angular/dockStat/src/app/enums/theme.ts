2d652bac89d4c29d919a28a3d6cabb2b8ea819b9
Anni Rauch <annkathrin_rauch@icloud.com>
2024-12-01 14:52:04 +0100

    added theme enum

---
+++ angular/dockStat/src/app/enums/theme.ts
@@ -0,0 +1,11 @@
+export enum Theme {
+    Light = "Light",
+    Nord = "Nord",
+    Pastel = "Pastel",
+    Business = "Business",
+    Dracula = "Dracula",
+    Sunset = "Sunset",
+    Night = "Night",
+    Forest = "Forest",
+    Amoled = "Amoled"
+}
